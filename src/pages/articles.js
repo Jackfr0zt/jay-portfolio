d0be81c4ee194233f44c57a098c6cd520e198431
Jay Amper <jayamper712@gmail.com>
2025-07-25 21:05:21 +0800

    Rename images

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -5,13 +5,13 @@ import AnimatedText from '@/components/AnimatedText';
 import Link from 'next/link';
 import Image from 'next/image';
 import { motion, useMotionValue } from 'framer-motion';
-import article1 from '../../public/images/articles/pagination component in reactjs.jpg';
-import article2 from '../../public/images/articles/create loading screen in react js.jpg';
-import article3 from '../../public/images/articles/form validation in reactjs using custom react hook.png';
-import article4 from '../../public/images/articles/smooth scrolling in reactjs.png';
-import article5 from '../../public/images/articles/create modal component in react using react portals.png';
-import article6 from '../../public/images/articles/todo list app built using react redux and framer motion.png';
-import article7 from '../../public/images/articles/What is Redux with easy explanation.png';
+import article1 from '../../public/images/articles/pagination-component-in-reactjs.jpg';
+import article2 from '../../public/images/articles/create-loading-screen-in-react-js.jpg';
+import article3 from '../../public/images/articles/form-validation-in-reactjs-using-custom-react-hook.png';
+import article4 from '../../public/images/articles/smooth-scrolling-in-reactjs.png';
+import article5 from '../../public/images/articles/create-modal-component-in-react-using-react-portals.png';
+import article6 from '../../public/images/articles/todo-list-app-built-using-react-redux-and-framer-motion.png';
+import article7 from '../../public/images/articles/What-is-Redux-with-easy-explanation.png';
 
 const FramerImage = motion(Image);
 
